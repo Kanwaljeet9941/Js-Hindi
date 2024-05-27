71343d451ba28bf6cb319b42f0d0d14b76ee52af
Kanwaljeet9941 <153882432+Kanwaljeet9941@users.noreply.github.com>
2024-05-27 04:34:12 +0000

    conversion operation

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "33"
+
+// console.log(typeof score);
+
+// let valueScore = Number(score)
+
+// console.log(typeof valueScore);
+
+// let newScore = "33abc"
+
+// let newValScore = Number(newScore)
+// console.log(typeof newValScore);
+// console.log(newValScore);
+
+// score = null
+// let ValScore = Number(score)
+// console.log(ValScore);
+
+// let score = true
+
+// let numname = Number(score)
+// console.log(numname);
+
+
+let score = 1
+
+let newScore = Boolean(score)
+console.log(newScore);
