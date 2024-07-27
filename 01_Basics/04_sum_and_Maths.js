8c51b4c8f1a8eb4e37b1fde47d5737f15b45d08e
Kanwaljeet9941 <153882432+Kanwaljeet9941@users.noreply.github.com>
2024-07-27 04:34:37 +0000

    math and num

---
+++ 01_Basics/04_sum_and_Maths.js
@@ -0,0 +1,21 @@
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const OtherNum = 123.89
+
+// console.log(OtherNum.toPrecision(4));
+
+/*******Maths*************/
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random()*(max -min +1))+min);
